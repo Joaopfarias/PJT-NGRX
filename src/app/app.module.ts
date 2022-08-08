b813e78e34ecb1a67b2477c70dcd98381d98504e
joaopfarias <joaop210601@gmail.com>
2022-08-08 17:12:47 -0300

    "First commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayContadorComponent } from './display-contador/display-contador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayContadorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-contador/display-contador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-contador',
+  templateUrl: './display-contador.component.html',
+  styleUrls: ['./display-contador.component.scss']
+})
+export class DisplayContadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
