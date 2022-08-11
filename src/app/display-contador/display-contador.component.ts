595645ab449327ca80d805e8ff3067abbadd75c7
joaopfarias <joaop210601@gmail.com>
2022-08-11 12:27:55 -0300

    Display Contador create

--- src/app/display-contador/display-contador.component.ts
+++ src/app/display-contador/display-contador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-display-contador',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-contador.component.scss']
 })
 export class DisplayContadorComponent implements OnInit {
+
+  @Input()
+  counter: number = 0;
 
   constructor() { }
 
