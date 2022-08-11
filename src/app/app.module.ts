e2c2654ccdc2d232b50f42ff1565366d7bff4386
joaopfarias <joaop210601@gmail.com>
2022-08-11 12:28:10 -0300

    StoreModule create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayContadorComponent } from './display-contador/display-contador.component';
+import { appReducer } from './store/app.state';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { DisplayContadorComponent } from './display-contador/display-contador.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ app: appReducer }),
   ],
   providers: [],
   bootstrap: [AppComponent]
